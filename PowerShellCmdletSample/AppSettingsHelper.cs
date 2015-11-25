ca88c8d9886b318538ea3c9063ae6b3168af6ed6
DRypa <dr.drypa.k@mail.ru>
2015-11-25 17:03:53 +0300

    PowerShell cmdlet example

---
+++ PowerShellCmdletSample/AppSettingsHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+
+namespace PowerShellCmdletSample
+{
+    internal class AppSettingsHelper
+    {
+        public static string ProxyDomain()
+        {
+            return GetSettingValue("ProxyDomain");
+        }
+
+        public static string ProxyPassword()
+        {
+            return GetSettingValue("ProxyPassword");
+        }
+
+        public static string ProxyUrl()
+        {
+            return GetSettingValue("ProxyUrl");
+        }
+
+        public static string ProxyUser()
+        {
+            return GetSettingValue("ProxyUser");
+        }
+
+        public static bool UseProxy()
+        {
+            KeyValueConfigurationCollection settings =
+                ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location).AppSettings.Settings;
+
+            string setting = GetSettingValue("UseProxy");
+            if (string.IsNullOrEmpty(setting))
+            {
+                return false;
+            }
+            bool result;
+            return bool.TryParse(setting, out result) && result;
+        }
+
+        private static string GetSettingValue(string settingKey)
+        {
+            KeyValueConfigurationCollection settings =
+                ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location).AppSettings.Settings;
+
+            KeyValueConfigurationElement setting = settings[settingKey];
+            return setting == null ? null : setting.Value;
+        }
+    }
+}
---
+++ PowerShellCmdletSample/GetBashRecord.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Management.Automation;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace PowerShellCmdletSample
+{
+    [Cmdlet(VerbsCommon.Get, "BashRecord")]
+    public class GetBashRecord : PSCmdlet
+    {
+        private const int Count = 436790;
+
+        private readonly string BashUrl = @"http://bash.im/quote/";
+
+        [Parameter(Mandatory = false, ValueFromPipeline = true, Position = 1)]
+        [ValidateRange(1, 436790)]
+        public int Number { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+            int quoteNum = Number == 0 ? random.Next(Count) : Number;
+            WriteObject(quoteNum);
+            string downloadString = GetPageFromServer(BashUrl + quoteNum);
+            string quote = GetQuote(downloadString);
+            ShowQuote(quote);
+        }
+
+        private string Decode(string html)
+        {
+            return html.Replace("&amp;", "&").Replace("&lt;", "<").Replace("&gt;", ">").Replace("&quot;", "\"").Replace("&apos;", "'");
+        }
+
+        private string GetPageFromServer(string url)
+        {
+            var client = new WebClient();
+            if (AppSettingsHelper.UseProxy())
+            {
+                IWebProxy proxy = new WebProxy(AppSettingsHelper.ProxyUrl());
+
+                proxy.Credentials = new NetworkCredential(AppSettingsHelper.ProxyUser(), AppSettingsHelper.ProxyPassword(), AppSettingsHelper.ProxyDomain());
+                client.Proxy = proxy;
+            }
+
+            string downloadString = client.DownloadString(url);
+            return downloadString;
+        }
+
+        private string GetQuote(string webPageString)
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml(webPageString);
+
+            HtmlNode node = document.DocumentNode.SelectSingleNode(".//*[@class='quote']/*[@class='text']");
+            return Decode(node.InnerText);
+        }
+
+        private void ShowQuote(string quote)
+        {
+            WriteObject(Environment.NewLine);
+            WriteObject(quote);
+            WriteObject(Environment.NewLine);
+        }
+    }
+}
